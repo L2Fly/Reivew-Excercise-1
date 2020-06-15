700dd4953948d57e3ee1aa1390d5bfaf6e03fd27
Laura Weyrick <65510690+L2Fly@users.noreply.github.com>
2020-06-14 22:50:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleChange (input) {
+  if (input.value <0) input.value=0;
+  if (input.value >100) input.value=100;
+}
+
+function handleChange2 (input) {
+  if (input.charAt ="i++" ) input.value="Enter more characters";
+  
+}
